8821164303b258f22eddcc8ad0869d583fa85c01
39754-Hari <39754.hexaware@gmail.com>
2018-07-02 16:45:36 +0530

    changes

--- routes.js
+++ routes.js
@@ -58,7 +58,7 @@ router.post('/validateUser',function(req, res){
 
 
 checkUserToken = function(req){
-	console.log(req);
+	console.log(req.originalDetectIntentRequest.payload);
 	var options ={
 		method: "POST",
 		url: config.generateTokenURL,
